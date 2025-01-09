33185831054d8d74b190718acc5be1670cd056ff
shiva <98257507+Shivuduu19@users.noreply.github.com>
2025-01-09 12:29:10 +0530

    add smooth

--- app/components/Hrchalleneges.jsx
+++ app/components/Hrchalleneges.jsx
@@ -2,10 +2,10 @@
 import React, { useEffect, useRef, useState } from 'react'
 import HrCards from './HrCards'
 import { useWindowSize } from '@uidotdev/usehooks'
-import { useMotionValueEvent, useScroll, useTransform } from "motion/react"
+import {motion, useMotionValueEvent,useScroll, useTransform } from "motion/react"
 const Hrchalleneges = () => {
     const [cardswidth,setCardsWidth]=useState(0)
-    const [cardstransval,setCardsTransVal]=useState(0)
+    // const [cardstransval,setCardsTransVal]=useState(0)
     const cardsRef=useRef(null)
     const containeref=useRef(null)
     const windowwidth=useWindowSize().width
@@ -15,7 +15,7 @@ const Hrchalleneges = () => {
         // console.log(windowwidth);
         const length=cardsRef.current
 
-console.log(length.getBoundingClientRect().width);
+// console.log(length.getBoundingClientRect().width);
 setCardsWidth(length.getBoundingClientRect().width)
     },[windowwidth,windowheiht])
 const {scrollYProgress}=useScroll({
@@ -27,27 +27,29 @@ const {scrollYProgress}=useScroll({
 //     console.log(x);
     
 // })
+const limit=cardswidth-windowwidth/2
+const cardsmoitonval=useTransform(scrollYProgress,[0,1],[0,limit])
+const negcardsmoitonval=useTransform(scrollYProgress,[0,1],[0,-limit])
+const halfnegcardsmoitonval=useTransform(scrollYProgress,[0,1],[0,-limit/2])
+// useMotionValueEvent(cardsmoitonval,'change',(x)=>{
+// setCardsTransVal(x)
+// // console.log(x);
+// }
 
-const cardsmoitonval=useTransform(scrollYProgress,[0,1],[0,cardswidth-windowwidth/2])
-useMotionValueEvent(cardsmoitonval,'change',(x)=>{
-setCardsTransVal(x)
-// console.log(x);
-}
-
-)
+// )
   return (
-    <section ref={containeref} className="c-section--dark-1 c-cards c-cards--5" data-animate-section style={{height: `${cardswidth-(windowheiht*.012)}px`}}>
-        <div style={{transform: `translateY(${cardstransval}px)`}} className="c-cards-inner" >
-        <div className="c-cards-title wrap wrap--max" style={{transform: `translateX(${-cardstransval/2}px)`}}>
+    <section ref={containeref} className="c-section--dark-1 c-cards c-cards--5" data-animate-section style={{height: `${(cardswidth-windowwidth/2)+(windowheiht)}px`}}>
+        <motion.div style={{y:cardsmoitonval}} className="c-cards-inner [will-change:transform]" >
+        <motion.div className="c-cards-title wrap wrap--max" style={{x: halfnegcardsmoitonval}}>
 			<div className="c-grid">
 				<div className="col-span-8 md:col-span-6">
 											<h2 className="ts-eyebrow ts-indicator" data-animate="" data-animate-y="50">OUR PRODUCTS &amp; SERVICES</h2>
 																<div className="ts-2" data-animate-title="" data-animate-delay="100"><div className="title-line"><div className="title-line-inner"><div className="title-line"><div className="title-line-inner">  Answers to </div></div></div></div><div className="title-line"><div className="title-line-inner"><div className="title-line"><div className="title-line-inner">your HR </div></div></div></div><div className="title-line"><div className="title-line-inner"><div className="title-line"><div className="title-line-inner">challenges </div></div></div></div></div>
 														</div>
 			</div>
-		</div>
+		</motion.div>
 
-          <div ref={cardsRef} className="c-cards-content" style={{transform: `translateX(${-cardstransval}px)`}}>
+          <motion.div ref={cardsRef} className="c-cards-content" style={{x: negcardsmoitonval}}>
 <div  className="c-cards-content-inner no-scrollbar">
     <HrCards/>
     <HrCards/>
@@ -60,9 +62,9 @@ setCardsTransVal(x)
     <HrCards/>
     {/* <HrCards/> */}
 </div>
-        </div>
+        </motion.div>
         
-        </div>
+        </motion.div>
     </section>
   )
 }
